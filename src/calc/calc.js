4c0647b05b651a13c20cf2effdac0f18df4f908c
Edwin Tangenberg <edwin.tangenberg@poldervalley.com>
2022-08-05 22:19:07 +0200

    added  initial calculation
    Signed-off-by: Edwin Tangenberg <edwin.tangenberg@poldervalley.com>

---
+++ src/calc/calc.js
@@ -0,0 +1,34 @@
+const calculate = (input, multipliers = [], structureTile = []) => {
+  const [s, t, r] = multipliers;
+  const mstars = s || 3;
+  const mterritories = t || 2;
+  const mresources = r || 1;
+  const {
+    stars: istars,
+    territories: iterritories,
+    resources: iresources,
+    factory,
+    structureScoringCount: scount,
+    coins: icoins,
+  } = input = {};
+  
+  const structureScoringCount = scount || 0;
+  const stars = istars || 0;
+  const territories = (iterritories + (factory ? 2 : 0 )) || 0;
+  const resources = iresources || 0;
+  const coins = icoins | 0;
+
+  const scoreStars = stars * mstars;
+  const scoreTerritories = territories * mterritories;
+  const scoreResources = Math.floor(resources / 2) * mresources;
+  const scoreStructures = structureTile[structureScoringCount] || 0;
+  const scoreCoins = coins;
+
+  return (
+    scoreStars +
+    scoreTerritories +
+    scoreResources +
+    scoreStructures +
+    scoreCoins
+  );
+};
